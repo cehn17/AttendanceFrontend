f99922ba53dcd6d5c11033a715163b66940aa1d7
Cesar Niveyro <cehn17@gmail.com>
2025-10-19 00:31:26 -0300

    ADD: Sidebars for Admin, Manager, Employee

---
+++ src/app/basic/basic-services/user-storage.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+
+const USER = 'att user'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserStorageService {
+
+  constructor() { }
+
+  static saveUser (user:any): void{
+    window.localStorage.removeItem(USER);
+    window.localStorage.setItem(USER, JSON.stringify(user));
+  }
+
+  static getUser(): any{
+    return JSON.parse(localStorage.getItem(USER));
+  }
+
+  static getUserId(): string{
+    const user = this.getUser();
+    if(user==null) {return '';}
+    return user.id;
+  }
+
+  static getUserProjectId(): string{
+    const user = this.getUser();
+    if(user==null) {return '';}
+    return user.projectid;
+  }
+
+  static getUserRole(): string{
+    const user = this.getUser();
+    if(user==null) {return '';}
+    return user.userRole;
+  }
+
+  static isAdminLoggedIn(): boolean{
+    if(this.getUserRole() == ''){
+      return false;
+    }
+    const role:string = this.getUserRole();
+    return role == 'ADMIN';
+  }
+
+  static isEmployeeLoggedIn(): boolean{
+    if(this.getUserRole() == ''){
+      return false;
+    }
+    const role:string = this.getUserRole();
+    return role == 'EMPLOYEE';
+  }
+
+  static isManagerLoggedIn(): boolean{
+    if(this.getUserRole() == ''){
+      return false;
+    }
+    const role:string = this.getUserRole();
+    return role == 'MANAGER';
+  }
+
+  static signOut(): void{
+    window.localStorage.removeItem(USER);
+  }
+}
